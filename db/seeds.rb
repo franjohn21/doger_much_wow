6e4ade2594c73fc32519318a4fbe615547235a50
Jess B Heron <jessbheron@gmail.com>
2014-10-10 16:11:00 -0700

    Such wow

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
 
-#10.times {User.create(username: Faker::Internet.user_name, password_hash: Faker::Internet.password)}
-#20.times {Doge.create(content: Faker::Company.bs, user_id: rand(1..10))}
+10.times {User.create(username: Faker::Internet.user_name, password_hash: Faker::Internet.password)}
+20.times {Doge.create(content: Faker::Company.bs, user_id: rand(1..10))}
 User.first.followees.create(username: Faker::Internet.user_name, password_hash: Faker::Internet.password)
